61b5080ddcc349ebefa62e8ebe81b588b6e945de
Sina Nasiri <sina.nasiri@datavirtuality.de>
2024-01-22 02:56:19 +0330

    Initial

--- github-profile/src/app/app.routes.ts
+++ github-profile/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {BlogComponent} from "./blog/blog.component";
+import {ExperienceComponent} from "./experience/experience.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {component: BlogComponent, path: 'blog'},
+  {component: ExperienceComponent, path: 'experience'},
+];
