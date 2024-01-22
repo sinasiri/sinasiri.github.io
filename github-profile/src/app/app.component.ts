12e2e49a6edea5744d9655258969a594e03c42da
Sina Nasiri <sina.nasiri@datavirtuality.de>
2024-01-22 20:39:31 +0330

    Adds Fonts and header component design.

--- github-profile/src/app/app.component.ts
+++ github-profile/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- github-profile/src/app/app.routes.ts
+++ github-profile/src/app/app.routes.ts
@@ -5,4 +5,7 @@ import {ExperienceComponent} from "./experience/experience.component";
 export const routes: Routes = [
   {component: BlogComponent, path: 'blog'},
   {component: ExperienceComponent, path: 'experience'},
+  {component: ExperienceComponent, path: 'projects'},
+  {component: ExperienceComponent, path: 'contact'},
+  {component: ExperienceComponent, path: 'education'},
 ];
