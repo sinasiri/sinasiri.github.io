a117260f7d9d7d0a63c3c8a06a8558ba0c1fc8b6
Sina Nasiri <sina.nasiri@datavirtuality.de>
2024-02-22 15:06:14 +0330

    Implements the blog pages.

--- github-profile/src/app/blog/blog.component.ts
+++ github-profile/src/app/blog/blog.component.ts
@@ -1,12 +1,26 @@
 import {Component} from '@angular/core';
+import {AnimationOptions, LottieComponent} from "ngx-lottie";
+import {NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [LottieComponent, NgFor],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss'
 })
 export class BlogComponent {
+  options: AnimationOptions = {
+    path: './assets/animations/blog.json',
+  };
 
+
+  blogs = [
+    {
+      date: '28 May, 2021',
+      title: 'Getting Started with Competetive programming',
+      imageUrl: './assets/images/blog/test.png',
+      href: 'https://google.com'
+    }
+  ]
 }
