208ad66ef4201e4690cddcd4c43fa1b567224895
Sina Nasiri <sina.nasiri@datavirtuality.de>
2024-10-23 17:04:56 +0330

    Adds wedding card :))

--- src/app/wedding/wedding.component.ts
+++ src/app/wedding/wedding.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-wedding',
@@ -7,7 +7,7 @@ import {Component} from '@angular/core';
   templateUrl: './wedding.component.html',
   styleUrl: './wedding.component.scss'
 })
-export class WeddingComponent {
+export class WeddingComponent implements AfterViewInit {
   openLink = () => {
     const url = 'https://www.google.com/maps?q=35.6327291,51.1900181';
     window.open(url, '_blank')
@@ -24,5 +24,15 @@ export class WeddingComponent {
     window.open('https://nshn.ir/e97bQKXxGxGy7o', '_blank')
   }
 
+
+  ngAfterViewInit(): void {
+    const video: HTMLVideoElement | null = document.querySelector('video');
+    if (video) {
+      video.muted = true;  // Ensure video is muted
+      video.play().catch(error => {
+        console.error('Video autoplay prevented', error);
+      });
+    }
+  }
 
 }
