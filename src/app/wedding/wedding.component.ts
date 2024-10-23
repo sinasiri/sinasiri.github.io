bfb7cce9b7204f9315d8f4fc989563c768c4119d
Sina Nasiri <sina.nasiri@datavirtuality.de>
2024-10-23 16:30:48 +0330

    Adds wedding card :))

--- src/app/wedding/wedding.component.ts
+++ src/app/wedding/wedding.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-wedding',
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './wedding.component.scss'
 })
 export class WeddingComponent {
+  openLink = (link: any) => {
+    window.open(link, '_blank')
+  }
+
+  openInWaze() {
+    const latitude = 35.6327291
+    const longitude = 51.1900181
+    const wazeUrl = `https://waze.com/ul?ll=${latitude},${longitude}&navigate=yes`;
+    window.open(wazeUrl)
+  }
+
+  openInNeshan() {
+    window.open('https://neshan.org/maps/places/_bQKkUOxGysW#c35.633-51.191-19z-0p', '_blank')
+  }
+
 
 }
