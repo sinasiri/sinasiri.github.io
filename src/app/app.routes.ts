6914f5e427722480bf0e9c487f9c083baf48dee6
Sina Nasiri <sina.nasiri@datavirtuality.de>
2024-11-18 17:47:23 +0330

    back to default app.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,12 @@ import {HomeComponent} from "./home/home.component";
 import {EducationComponent} from "./education/education.component";
 import {ContactComponent} from "./contact/contact.component";
 import {ProjectsComponent} from "./projects/projects.component";
-import {WeddingComponent} from "./wedding/wedding.component";
 
 export const routes: Routes = [
-  {component: WeddingComponent, path: ''},
-  // {component: BlogComponent, path: 'blog'},
-  // {component: ExperienceComponent, path: 'experience'},
-  // {component: ProjectsComponent, path: 'projects'},
-  // {component: ContactComponent, path: 'contact'},
-  // {component: EducationComponent, path: 'education'},
+  {component: HomeComponent, path: ''},
+  {component: BlogComponent, path: 'blog'},
+  {component: ExperienceComponent, path: 'experience'},
+  {component: ProjectsComponent, path: 'projects'},
+  {component: ContactComponent, path: 'contact'},
+  {component: EducationComponent, path: 'education'},
 ];
