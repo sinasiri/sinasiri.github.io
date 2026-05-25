5f7289e4354a15f54a713b3bef30c80f3fe979ec
S.Nasiri <S.Nasiri>
2026-05-25 17:55:07 +0330

    Fixed experience section.

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -30,8 +30,8 @@ export class ExperienceComponent implements OnInit {
                 company: 'Data Virtuality GMBH',
                 date: 'September 2020 - till now',
                 location: 'Germany (Remote)'
-            },
-            right: {
+            }
+            , right: {
                 role: 'Front-End Developer',
                 description: 'Working as a Senior Angular Developer'
             }
@@ -41,10 +41,54 @@ export class ExperienceComponent implements OnInit {
                 company: 'ibpay.ir',
                 date: 'February 2020 - October 2020',
                 location: 'Iran'
-            },
-            right: {
+            }
+            , right: {
                 role: 'Front-End Developer',
                 description: 'Worked as a Senior Angular Developer'
+            }
+        },
+        {
+            left: {
+                company: 'Nicico (National Iranian Copper Industries)',
+                date: 'February 2018 - February 2020',
+                location: 'Iran'
+            }
+            , right: {
+                role: 'Front-End Developer',
+                description: 'Worked as a Senior Angular Developer'
+            }
+        },
+        {
+            left: {
+                company: 'Maxsa',
+                date: 'July 2018 - February 2019',
+                location: 'Iran'
+            }
+            , right: {
+                role: 'Front-End Developer',
+                description: 'Worked as an Angular Developer'
+            }
+        },
+        {
+            left: {
+                company: 'Fusoft',
+                date: 'March 2016 - June 2018',
+                location: 'Iran'
+            }
+            , right: {
+                role: 'Full-Stack Web Developer',
+                description: 'Worked as a Full-Stack .Net and Angular developer'
+            }
+        },
+        {
+            left: {
+                company: 'Pasargad',
+                date: '2016',
+                location: 'Iran'
+            }
+            , right: {
+                role: 'Mathematics Teacher',
+                description: 'Mathematics and geometry teacher in high school'
             }
         }
     ];
